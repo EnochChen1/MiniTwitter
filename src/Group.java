7ce93d7d23180f554890acba0a0e42dd7f52d82e
EnochChen1 <enoch.chen.personal@gmail.com>
2023-11-11 22:26:28 -0800

    Finished creating user, groups, observers, subjects

--- src/Group.java
+++ src/Group.java
@@ -1,15 +1,94 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Group {
-    private String groupName;
-    private List<User> members;
-    private List<Group> subGroups;
+public class Group extends AbstractUser {
 
-    public Group(String groupName) {
-        this.groupName = groupName;
-        this.members = new ArrayList<>();
-        this.subGroups = new ArrayList<>();
+    private Map<String,AbstractUser> groups;
+
+    public Group(String userName) {
+        super(userName);
+        groups = new HashMap<String,AbstractUser>();
+    }
+
+    public Map<String,AbstractUser> getGroups() {
+        return groups;
+    }
+
+    public AbstractUser addUserInGroup(AbstractUser user) {
+        if (!this.contains(user.getUserName())) {
+            this.groups.put(user.getUserName(), user);
+        }
+        return this;
+    }
+
+    @Override
+    public boolean contains(String userName) {
+        boolean contains = false;
+        for (AbstractUser user : groups.values()) {
+            if (user.contains(userName)) {
+                contains = true;
+            }
+        }
+        return contains;
+    }
+
+    @Override
+    public int getUserCount() {
+        int count = 0;
+        for (AbstractUser user : this.groups.values()) {
+            count += user.getUserCount();
+        }
+        return count;
+    }
+
+    @Override
+    public int getGroupCount() {
+        int count = 0;
+        for (AbstractUser user : this.groups.values()) {
+            if (user.getClass() == Group.class) {
+                ++count;
+                count += user.getGroupCount();
+            }
+        }
+        return count;
+    }
+
+
+    @Override
+    public int getMessageCount() {
+        int msgCount = 0;
+        for (AbstractUser user : this.groups.values()) {
+            msgCount += user.getMessageCount();
+        }
+        return msgCount;
+    }
+
+
+    @Override
+    public void update(Subject subject) {
+        for (AbstractUser user : groups.values()) {
+            ((Observer) user).update(subject);
+        }
+    }
+
+
+
+    @Override
+    public void accept(Visitor visitor) {
+        for (AbstractUser user : groups.values()) {
+            user.accept(visitor);
+        }
+        visitor.visitGroup(this);
+    }
+
+    private boolean containsGroupUser() {
+        boolean containsGroup = false;
+        for (AbstractUser user : this.groups.values()) {
+            if (user.getClass() == Group.class) {
+                containsGroup = true;
+            }
+        }
+        return containsGroup;
     }
 
 }
--- src/User.java
+++ src/User.java
@@ -1,26 +1,115 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
-public class User {
-    private String userName;
-    private List<String> followers;
-    private List<String> followings;
-    private List<Tweet> tweets;
+public class User extends AbstractUser implements Subject {
+    private static final List<String> POSITIVE_WORDS = Arrays.asList("good","great", "excellent"
+    , "nice", "awesome");
+
+    private Map<String, Observer> followers;
+    private Map<String, Subject> following;
+    private List<String> newsFeed;
+    private int positiveMessageCount;
+    private String latestMessage;
 
     public User(String userName) {
-        this.userName = userName;
-        this.followers = new ArrayList<>();
-        this.followings = new ArrayList<>();
-        this.tweets = new ArrayList<>();
+        super(userName);
+        followers = new HashMap<String, Observer>();
+        following = new HashMap<String, Subject>();
+        
+        followers.put(this.getUserName(), this);
+
+        newsFeed = new ArrayList<String>();
     }
 
+    public Map<String, Observer> getFollowers() {
+        return followers;
+    }
 
-    public void followUser(String following) {
-        if (!followings.contains(following)) {
-            followings.add(following);
-        }
+    public Map<String, Subject> getFollowing() {
+        return following;
+    }
+
+    public List<String> getNewsFeed() {
+        return newsFeed;
     }
 
     
 
+    public void Tweet(String message) {
+        this.latestMessage = message;
+        this.setMessageCount(this.getMessageCount()+1);
+        if(isPositiveMessage(message)) {
+            ++positiveMessageCount;
+        }
+
+        notifyObservers();
+    }
+
+    public String getLatestMessage() {
+        return this.latestMessage;
+    }
+
+    private boolean isPositiveMessage(String message) {
+        boolean positive = false;
+        message = message.toLowerCase();
+        for (String word : POSITIVE_WORDS) {
+            if (message.contains(word)) {
+                positive = true;
+            }
+        }
+        return positive;
+    }
+
+    @Override
+    public void update(Subject subject) {
+        newsFeed.add(0, (((User) subject).getUserName()
+        +" : " + ((User) subject).getLatestMessage()));
+    }
+
+    @Override
+    public boolean contains(String userName) {
+        return this.getUserName().equals(userName);
+    }
+
+    @Override
+    public int getUserCount() {
+        return 1;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return 0;
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visitUser(this);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for(Observer obs : followers.values()) {
+            obs.update(this);
+        }
+    }
+
+    @Override
+    public void attach(Observer observer) {
+        addFollower(observer);
+    }
+
+    private void addFollower(Observer user) {
+        this.getFollowers().put(((AbstractUser) user).getUserName(), user);
+        ((User) user).addUsertoFollow(this);
+    }
+
+    private void addUsertoFollow(Subject toFollow){
+        if (toFollow.getClass() == User.class) {
+            getFollowing().put(((AbstractUser) toFollow).getUserName(), toFollow);
+        }
+    } 
+    
 }
